faff5534e542aa79af4c51a8be4acd9062ceb16c
Jarquevious <jarquevious@gmail.com>
2019-12-04 23:29:55 -0800

    2nd times a charm

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 from bson.objectid import ObjectId
-client = MongoClient()
-db = client.Playlister
+
+import os
+host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Playlister')
+client = MongoClient(host=f'{host}?retryWrites=false')
+db = client.get_default_database()
 playlists = db.playlists
+
 
 app = Flask(__name__)
 def video_url_creator(id_lst):
