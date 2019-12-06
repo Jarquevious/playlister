1de6a5a75d77baf1567579cc094f133312f9843a
Jarquevious <jarquevious@gmail.com>
2019-12-05 22:07:34 -0800

    Comment form added

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 from bson.objectid import ObjectId
-
 import os
 host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Playlister')
 client = MongoClient(host=f'{host}?retryWrites=false')
